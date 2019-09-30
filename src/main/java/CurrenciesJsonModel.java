91f75e892254a30fd12c64caaccfe500030c425d
Skeimor <nemchenko-1999@mail.ru>
2019-09-30 10:35:31 +0500

    Added json models for currencies

---
+++ src/main/java/CurrenciesJsonModel.java
@@ -0,0 +1,9 @@
+import java.util.Map;
+
+public class CurrenciesJsonModel {
+    private String Date;
+    private String PreviousDate;
+    private String PreviousURL;
+    private String TimeStamp;
+    private Map<String, CurrencyDataModel> Valute;
+}
---
+++ src/main/java/CurrencyDataModel.java
@@ -0,0 +1,9 @@
+public class CurrencyDataModel {
+    private String ID;
+    private String NumCode;
+    private  String CharCode;
+    private String Nominal;
+    private String Name;
+    private String Value;
+    private String Previous;
+}
