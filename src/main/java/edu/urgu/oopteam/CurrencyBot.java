0c42997a35677982d0df0239f5932a3bccb01e62
Skeimor <nemchenko-1999@mail.ru>
2019-10-14 13:45:17 +0500

    1) Made auto-update of currModel every 4 hours 2) Little refactoring

--- src/main/java/edu/urgu/oopteam/CurrencyBot.java
+++ src/main/java/edu/urgu/oopteam/CurrencyBot.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.urgu.oopteam.models.CurrenciesJsonModel;
 import edu.urgu.oopteam.services.ConfigurationSettings;
-import edu.urgu.oopteam.services.FileService;
 import edu.urgu.oopteam.services.WebService;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -13,7 +12,9 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.io.*;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Logger;
 
 public class CurrencyBot extends TelegramLongPollingBot {
@@ -21,21 +22,27 @@ public class CurrencyBot extends TelegramLongPollingBot {
             "\nЯ могу показывать курсы валют. Используй команды ниже:" +
             "\n/help - показать это сообщение и список возможных команд" +
             "\n/curr {код валюты} - показать курс указанной валюты к рублю";
+    private final static String JSON_PAGE_ADDRESS = "https://www.cbr-xml-daily.ru/daily_json.js";
     private ConfigurationSettings configSettings;
     private CurrenciesJsonModel currModel;
     private final static Logger LOGGER = Logger.getLogger(CurrencyBot.class.getCanonicalName());
 
-    public CurrencyBot(ConfigurationSettings settings){
-        super();
-        configSettings = settings;
-    }
-    public CurrencyBot(ConfigurationSettings settings, DefaultBotOptions botOptions) throws Exception {
+    public CurrencyBot(ConfigurationSettings settings, DefaultBotOptions botOptions) {
         super(botOptions);
         configSettings = settings;
 
-        var mapper = new ObjectMapper();
-        var webPage = WebService.getPageContent("https://www.cbr-xml-daily.ru/daily_json.js", "UTF-8");
-        currModel = mapper.readValue(webPage, CurrenciesJsonModel.class);
+        var jsonUpdateTimer = new Timer();
+        var jsonUpdateTask = new TimerTask() {
+          @Override
+          public void run(){
+              tryUpdateJsonModel();
+              /* Потом тут в случае неудачи нужно будет отправить сообщение всем пользователям бота,
+               что обновить данные не удалось, и они получат несколько устаревшие данные.
+                Для реализации этого нужно хранить где-то(например в json) все id пользователей, которые уже
+                используют бота */
+          }
+        };
+        jsonUpdateTimer.scheduleAtFixedRate(jsonUpdateTask, new Date(), 1000 * 60 * 60 * 4);
     }
 
     @Override
@@ -54,7 +61,6 @@ public class CurrencyBot extends TelegramLongPollingBot {
                     return;
                 }
 
-                /* Позже тут должна быть проверка на то, что пора обновить json файл */
                 var exRate = currModel.getExchangeRate(message[1]);
                 if (exRate != null){
                     sendMessage(chatID,exRate + " RUB");
@@ -77,6 +83,26 @@ public class CurrencyBot extends TelegramLongPollingBot {
         }
     }
 
+    /**
+     * Если возможно, обновляет поле currModel.
+     * @return Возвращает true или false, в зависимости от того, удалось ли удачно обновить модель
+     */
+    private boolean tryUpdateJsonModel() {
+        var mapper = new ObjectMapper();
+        try {
+            var webPage = WebService.getPageContent(JSON_PAGE_ADDRESS, "UTF-8");
+            currModel = mapper.readValue(webPage, CurrenciesJsonModel.class);
+            return true;
+        } catch (JsonProcessingException jException){
+            LOGGER.info("Error while building CurrenciesJsonModel instance");
+            jException.printStackTrace();
+        } catch (Exception e){
+            LOGGER.info("Error while downloading page");
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     @Override
     public String getBotUsername() {
         return configSettings.getBotUserName();
@@ -85,17 +111,5 @@ public class CurrencyBot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return configSettings.getBotToken();
-    }
-
-    private CurrenciesJsonModel getCurrModelFromJson(String content) {
-        var mapper = new ObjectMapper();
-        try {
-            return mapper.readValue(content, CurrenciesJsonModel.class);
-        } catch (JsonProcessingException e) {
-            LOGGER.info("Error while parsing json file");
-            e.printStackTrace();
-//            System.exit(-1);
-        }
-        return null;
     }
 }
--- src/main/java/edu/urgu/oopteam/Main.java
+++ src/main/java/edu/urgu/oopteam/Main.java
@@ -8,7 +8,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi botsApi = new TelegramBotsApi();
         var settings = new ConfigurationSettings();
